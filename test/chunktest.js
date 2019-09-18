14c0eb566a0d540019db4a43f2ef4a71c15d6e39
lolicnea <loliconkea@gmail.com>
2019-09-18 15:25:19 +0800

    add chunk and pass test

---
+++ test/chunktest.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert')
+const _ = require('../src/chunk')
+var expect = require('chai').expect;
+
+describe('chunk', function() {
+  describe('chunk', function() {
+    it("_.chunk(['a', 'b', 'c', 'd'], 2) equalTo [ [ 'a', 'b' ], [ 'c', 'd' ] ]", function() {
+      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 2), [ [ 'a', 'b'], ['c' , 'd' ] ])
+    });
+    it("_.chunk(['a', 'b', 'c', 'd'], 3) equalTo [ [ 'a', 'b', 'c' ], [ 'd' ] ]", function() {
+      assert.deepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [ [ 'a', 'b', 'c' ], [ 'd' ] ])
+    });
+    it("_.chunk(['a', 'b', 'c', 'd'], 3) notEqualTo [ [ 'a', 'b'], ['c' , 'd' ] ]", function() {
+      assert.notDeepStrictEqual(_.chunk(['a', 'b', 'c', 'd'], 3), [ [ 'a', 'b'], ['c' , 'd' ] ])
+    });
+  });
+});
